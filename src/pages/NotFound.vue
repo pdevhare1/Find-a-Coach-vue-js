7da065cdd8183fed64f97f773c8c977cd6be03c2
Prathamesh Devhare <prathamesh.kali@gmail.com>
2023-07-12 21:12:42 +0530

    final

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,5 +1,11 @@
 <template>
   <section>
-    <h1>Coaches Detail</h1>
+    <base-card>
+      <h2>Page not found</h2>
+      <p>
+        This page could not be found - maybe check out all our
+        <router-link to="/coaches">coaches</router-link>.
+      </p>
+    </base-card>
   </section>
 </template>
